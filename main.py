400fa5382900d9586ce369ac2282cba018e5ae08
NakatsukaKyohei <sansankyosan@gmail.com>
2024-09-01 21:42:25 +0900

    [Add] キラーを認識しアドオンを認識する機能の追加

--- main.py
+++ main.py
@@ -2,29 +2,43 @@ import cv2
 import os
 import numpy as np
 
-template_file_path = "./database/add_on/cannibal"
-base_image_path = './database/search_image.jpg'
-first_addon_location_left_up = (550, 794)
-first_addon_location_right_down = (591, 835)
-second_addon_location_left_up = (597, 794)
-second_addon_location_right_down = (637, 835)
+killer_template_path = "./database/killer/"
+addon_template_path = "./database/addon/"
+base_image_path = './database/test.jpg'
 # n個目のアドオン / 左上 or 右下 / x座標 or y座標
 addon_location = [[[550, 794], [591, 835]], [[597, 794], [637, 835]]]
+killer_location = [[484, 794], [524, 835]] 
 
 def main():
-    killer_addon_path_list = os.listdir(template_file_path)
     base_image = imread(base_image_path)
 
-    first_addon_base = base_image[addon_location[0][0][1]:addon_location[0][1][1], addon_location[0][0][0]:addon_location[0][1][0]]
-    second_addon_base = base_image[addon_location[1][0][1]:addon_location[1][1][1], addon_location[1][0][0]:addon_location[1][1][0]]
+    killer_base_image = base_image[killer_location[0][1]:killer_location[1][1], killer_location[0][0]:killer_location[1][0]]
+    killer_type = get_most_reliable_template(killer_template_path, os.listdir(killer_template_path), killer_base_image)
+    print("キラー: {}".format(killer_type))
 
-    first_addon = get_most_reliable_addon(killer_addon_path_list, first_addon_base)
-    second_addon = get_most_reliable_addon(killer_addon_path_list, second_addon_base)
+    first_addon_base_image = base_image[addon_location[0][0][1]:addon_location[0][1][1], addon_location[0][0][0]:addon_location[0][1][0]]
+    second_addon_base_image = base_image[addon_location[1][0][1]:addon_location[1][1][1], addon_location[1][0][0]:addon_location[1][1][0]]
+
+    addon_path_list = os.listdir(addon_template_path + killer_type[0])
+    first_addon = get_most_reliable_template(addon_template_path + killer_type[0], addon_path_list, first_addon_base_image)
+    second_addon = get_most_reliable_template(addon_template_path + killer_type[0], addon_path_list, second_addon_base_image)
     print("使用アドオン1: {}".format(first_addon[0]))
     print("使用アドオン2: {}".format(second_addon[0]))
 
+    killer_top_left = (killer_location[0][0] + killer_type[1][1][0], killer_location[0][1] + killer_type[1][1][1])
+    killer_bottom_right = (killer_top_left[0] + 37, killer_top_left[1] + 37)
+    cv2.rectangle(base_image, killer_top_left, killer_bottom_right, (255, 255, 0), 2)
+
+    first_addon_top_left = (addon_location[0][0][0] + first_addon[1][1][0], addon_location[0][0][1] + first_addon[1][1][1])
+    first_addon_bottom_right = (first_addon_top_left[0] + 37, first_addon_top_left[1] + 37)
+    cv2.rectangle(base_image, first_addon_top_left, first_addon_bottom_right, (255, 255, 0), 2)
+
+    second_addon_top_left = (addon_location[1][0][0] + second_addon[1][1][0], addon_location[1][0][1] + first_addon[1][1][1])
+    second_addon_bottom_right = (second_addon_top_left[0] + 37, second_addon_top_left[1] + 37)
+    cv2.rectangle(base_image, second_addon_top_left, second_addon_bottom_right, (255, 255, 0), 2)
+
     # 画像を表示
-    cv2.imshow('base_image', second_addon_base)
+    cv2.imshow('base_image', base_image)
     cv2.waitKey(0) 
     cv2.destroyAllWindows()
 
@@ -37,10 +51,10 @@ def imread(filename, flags=cv2.IMREAD_COLOR, dtype=np.uint8):
         print(e)
         return None
 
-def get_most_reliable_addon(template_path_list, base_image):
+def get_most_reliable_template(base_path, template_path_list, base_image):
     results = {}
     for i in template_path_list:
-        path = "{}/{}".format(template_file_path, i)
+        path = "{}/{}".format(base_path, i)
         template = imread(path)
         result = template_match(template, base_image)
         basename = i.split('.')[0]
@@ -49,7 +63,6 @@ def get_most_reliable_addon(template_path_list, base_image):
     return results[0]
 
 def template_match(template, base_image):
-
     result = cv2.matchTemplate(base_image, template, cv2.TM_CCORR_NORMED)
 
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
