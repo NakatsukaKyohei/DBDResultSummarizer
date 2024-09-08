a0fc3b56cee8658907475c57f654192586b18940
NakatsukaKyohei <sansankyosan@gmail.com>
2024-09-08 15:52:57 +0900

    [Add] サバイバーパークの画像認識の追加

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import os
 import numpy as np
 
 killer_perk_template_path = "./database/killer/perk"
-killer_template_path = "./database/killer/icon"
-addon_template_path = "./database/killer/addon/"
+killer_icon_template_path = "./database/killer/icon"
+killer_addon_template_path = "./database/killer/addon/"
 surviver_status_template_path = "./database/surviver/status/"
+surviver_perk_template_path = "./database/surviver/perk"
 base_image_path = './database/result/381210_20240907153516_1.png'
 # base_image_path = './database/killer_reference/スカルマーチャント.png'
 
 # アイコンの位置ずれを想定した位置バッファ
-location_buffer = 10
+location_buffer = 16
 
 # パークアイコンの位置
 killer_perk_location = [
@@ -39,26 +40,30 @@ surviver_status_location = [
     [[180 - location_buffer, 619 - location_buffer], [212 + location_buffer, 647 + location_buffer]]
 ]
 
+# サバイバーパークの位置(左上)
+# 列ごとのx座標 / 行ごとのy座標
+surviver_perk_location = [
+    [187, 243, 298, 354],
+    [310, 430, 551, 672]
+]
+
+
 def main():
     base_image = imread(base_image_path)
-    preprocessed_image = image_preprocess(base_image)
-
-    # 画像を表示
-    cv2.imshow('base_image', preprocessed_image)
-    cv2.waitKey(0) 
-    cv2.destroyAllWindows()
+    preprocessed_image_for_perk = image_preprocess(base_image, 2)
+    preprocessed_image_for_other = image_preprocess(base_image)
 
     # キラーパークの画像認識
-    first_killer_perk_base_image = preprocessed_image[killer_perk_location[0][0][1]:killer_perk_location[0][1][1], killer_perk_location[0][0][0]:killer_perk_location[0][1][0]]
-    second_killer_perk_base_image = preprocessed_image[killer_perk_location[1][0][1]:killer_perk_location[1][1][1], killer_perk_location[1][0][0]:killer_perk_location[1][1][0]]
-    third_killer_perk_base_image = preprocessed_image[killer_perk_location[2][0][1]:killer_perk_location[2][1][1], killer_perk_location[2][0][0]:killer_perk_location[2][1][0]]
-    fourth_killer_perk_base_image = preprocessed_image[killer_perk_location[3][0][1]:killer_perk_location[3][1][1], killer_perk_location[3][0][0]:killer_perk_location[3][1][0]]
+    first_killer_perk_base_image = preprocessed_image_for_perk[killer_perk_location[0][0][1]:killer_perk_location[0][1][1], killer_perk_location[0][0][0]:killer_perk_location[0][1][0]]
+    second_killer_perk_base_image = preprocessed_image_for_perk[killer_perk_location[1][0][1]:killer_perk_location[1][1][1], killer_perk_location[1][0][0]:killer_perk_location[1][1][0]]
+    third_killer_perk_base_image = preprocessed_image_for_perk[killer_perk_location[2][0][1]:killer_perk_location[2][1][1], killer_perk_location[2][0][0]:killer_perk_location[2][1][0]]
+    fourth_killer_perk_base_image = preprocessed_image_for_perk[killer_perk_location[3][0][1]:killer_perk_location[3][1][1], killer_perk_location[3][0][0]:killer_perk_location[3][1][0]]
 
     killer_perk_path_list = os.listdir(killer_perk_template_path)
-    first_killer_perk = get_most_reliable_template(killer_perk_template_path, killer_perk_path_list, first_killer_perk_base_image)
-    second_killer_perk = get_most_reliable_template(killer_perk_template_path, killer_perk_path_list, second_killer_perk_base_image)
-    third_killer_perk = get_most_reliable_template(killer_perk_template_path, killer_perk_path_list, third_killer_perk_base_image)
-    fourth_killer_perk = get_most_reliable_template(killer_perk_template_path, killer_perk_path_list, fourth_killer_perk_base_image)
+    first_killer_perk = get_most_reliable_template(killer_perk_template_path, killer_perk_path_list, first_killer_perk_base_image, 2)
+    second_killer_perk = get_most_reliable_template(killer_perk_template_path, killer_perk_path_list, second_killer_perk_base_image, 2)
+    third_killer_perk = get_most_reliable_template(killer_perk_template_path, killer_perk_path_list, third_killer_perk_base_image, 2)
+    fourth_killer_perk = get_most_reliable_template(killer_perk_template_path, killer_perk_path_list, fourth_killer_perk_base_image, 2)
 
     print("キラーパーク1: {}".format(first_killer_perk[0]))
     print("キラーパーク2: {}".format(second_killer_perk[0]))
@@ -71,19 +76,19 @@ def main():
     rectangle(base_image, killer_perk_location[3][0][0], killer_perk_location[3][0][1], killer_perk_location[3][1][0], killer_perk_location[3][1][1])
 
     # キラーの画像認識
-    killer_base_image = preprocessed_image[killer_location[0][1]:killer_location[1][1], killer_location[0][0]:killer_location[1][0]]
-    killer_type = get_most_reliable_template(killer_template_path, os.listdir(killer_template_path), killer_base_image)
-    print("キラー: {}".format(killer_type))
+    killer_base_image = preprocessed_image_for_other[killer_location[0][1]:killer_location[1][1], killer_location[0][0]:killer_location[1][0]]
+    killer_type = get_most_reliable_template(killer_icon_template_path, os.listdir(killer_icon_template_path), killer_base_image)
+    print("キラー: {}".format(killer_type[0]))
 
     rectangle(base_image, killer_location[0][0], killer_location[0][1], killer_location[1][0], killer_location[1][1])
 
     # アドオンの画像認識
-    first_addon_base_image = preprocessed_image[addon_location[0][0][1]:addon_location[0][1][1], addon_location[0][0][0]:addon_location[0][1][0]]
-    second_addon_base_image = preprocessed_image[addon_location[1][0][1]:addon_location[1][1][1], addon_location[1][0][0]:addon_location[1][1][0]]
+    first_addon_base_image = preprocessed_image_for_other[addon_location[0][0][1]:addon_location[0][1][1], addon_location[0][0][0]:addon_location[0][1][0]]
+    second_addon_base_image = preprocessed_image_for_other[addon_location[1][0][1]:addon_location[1][1][1], addon_location[1][0][0]:addon_location[1][1][0]]
 
-    addon_path_list = os.listdir(addon_template_path + killer_type[0])
-    first_addon = get_most_reliable_template(addon_template_path + killer_type[0], addon_path_list, first_addon_base_image)
-    second_addon = get_most_reliable_template(addon_template_path + killer_type[0], addon_path_list, second_addon_base_image)
+    addon_path_list = os.listdir(killer_addon_template_path + killer_type[0])
+    first_addon = get_most_reliable_template(killer_addon_template_path + killer_type[0], addon_path_list, first_addon_base_image)
+    second_addon = get_most_reliable_template(killer_addon_template_path + killer_type[0], addon_path_list, second_addon_base_image)
     print("使用アドオン1: {}".format(first_addon[0]))
     print("使用アドオン2: {}".format(second_addon[0]))
 
@@ -91,10 +96,10 @@ def main():
     rectangle(base_image, addon_location[1][0][0], addon_location[1][0][1], addon_location[1][1][0], addon_location[1][1][1])
 
     # サバイバーステータスの画像認識
-    first_surviver_base_image = preprocessed_image[surviver_status_location[0][0][1]:surviver_status_location[0][1][1], surviver_status_location[0][0][0]:surviver_status_location[0][1][0]]
-    second_surviver_base_image = preprocessed_image[surviver_status_location[1][0][1]:surviver_status_location[1][1][1], surviver_status_location[1][0][0]:surviver_status_location[1][1][0]]
-    third_surviver_base_image = preprocessed_image[surviver_status_location[2][0][1]:surviver_status_location[2][1][1], surviver_status_location[2][0][0]:surviver_status_location[2][1][0]]
-    fourth_surviver_base_image = preprocessed_image[surviver_status_location[3][0][1]:surviver_status_location[3][1][1], surviver_status_location[3][0][0]:surviver_status_location[3][1][0]]
+    first_surviver_base_image = preprocessed_image_for_other[surviver_status_location[0][0][1]:surviver_status_location[0][1][1], surviver_status_location[0][0][0]:surviver_status_location[0][1][0]]
+    second_surviver_base_image = preprocessed_image_for_other[surviver_status_location[1][0][1]:surviver_status_location[1][1][1], surviver_status_location[1][0][0]:surviver_status_location[1][1][0]]
+    third_surviver_base_image = preprocessed_image_for_other[surviver_status_location[2][0][1]:surviver_status_location[2][1][1], surviver_status_location[2][0][0]:surviver_status_location[2][1][0]]
+    fourth_surviver_base_image = preprocessed_image_for_other[surviver_status_location[3][0][1]:surviver_status_location[3][1][1], surviver_status_location[3][0][0]:surviver_status_location[3][1][0]]
 
     surviver_status_path_list = os.listdir(surviver_status_template_path)
     first_surviver_status = get_most_reliable_template(surviver_status_template_path, surviver_status_path_list, first_surviver_base_image)
@@ -111,6 +116,27 @@ def main():
     rectangle(base_image, surviver_status_location[1][0][0], surviver_status_location[1][0][1], surviver_status_location[1][1][0], surviver_status_location[1][1][1])
     rectangle(base_image, surviver_status_location[2][0][0], surviver_status_location[2][0][1], surviver_status_location[2][1][0], surviver_status_location[2][1][1])
     rectangle(base_image, surviver_status_location[3][0][0], surviver_status_location[3][0][1], surviver_status_location[3][1][0], surviver_status_location[3][1][1])
+
+    # サバイバーパークの画像認識
+    surviver_perks = []
+    surviver_perk_path_list = os.listdir(surviver_perk_template_path)
+    # i人目のパーク
+    for i in range(4):
+        surviver_perks_i = []
+        # j個目のパーク
+        for j in range(4):
+            left_top_x = surviver_perk_location[0][j] - location_buffer
+            left_top_y = surviver_perk_location[1][i] - location_buffer
+            bottom_right_x = left_top_x + 46 + (location_buffer * 2)
+            bottom_right_y = left_top_y + 46 + (location_buffer * 2)
+            
+            perk_base_image = preprocessed_image_for_perk[left_top_y:bottom_right_y, left_top_x:bottom_right_x]
+
+            rectangle(base_image, left_top_x, left_top_y, bottom_right_x, bottom_right_y)
+
+            surviver_perks_i.append(get_most_reliable_template(surviver_perk_template_path, surviver_perk_path_list, perk_base_image, 2))
+        print("{}人目サバイバーパーク: {} / {} / {} / {}".format(i+1, surviver_perks_i[0][0], surviver_perks_i[1][0], surviver_perks_i[2][0], surviver_perks_i[3][0]))
+
 
     show_image(base_image)
 
@@ -129,16 +155,15 @@ def image_preprocess(img, C=0.5):
     threshold = cv2.adaptiveThreshold(gaussian, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, C)
     return threshold
 
-def get_most_reliable_template(base_path, template_path_list, base_image):
+def get_most_reliable_template(base_path, template_path_list, base_image, C=None):
     results = {}
     for i in template_path_list:
         path = "{}/{}".format(base_path, i)
-        template = image_preprocess(imread(path))
+        template = image_preprocess(imread(path), C)
         result = template_match(template, base_image)
         basename = i.split('.')[0]
         results[basename] = result
     results = sorted(results.items(), key=lambda x:x[1], reverse=True)
-    # print(results)
 
     return results[0]
 
